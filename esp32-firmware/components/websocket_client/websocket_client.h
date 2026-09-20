20122e7fec8e9f3e120b0f947291f5f7449ffe0d
Debanjan Gain <debanjangain1@gmail.com>
2026-09-20 22:28:06 +0530

    websocket_client.h

--- esp32-firmware/components/websocket_client/websocket_client.h
+++ esp32-firmware/components/websocket_client/websocket_client.h
@@ -1,7 +1,14 @@
 #pragma once
+
 #include <stdint.h>
 #include <stddef.h>
 
-void websocket_client_start(const char* uri);
-void websocket_client_send(const uint8_t* data, size_t len);
-int websocket_client_receive(uint8_t* buffer, size_t max_len);
+void websocket_client_start(const char *uri);
+
+void websocket_client_send(
+    const uint8_t *data,
+    size_t len);
+
+int websocket_client_receive(
+    uint8_t *buffer,
+    size_t max_len);
