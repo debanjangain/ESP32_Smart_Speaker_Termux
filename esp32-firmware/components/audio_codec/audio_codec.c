c65ff200fbc3d8fdf28bea78d5d9f777103b037f
Debanjan Gain <debanjangain1@gmail.com>
2026-09-20 22:24:30 +0530

    audio_codec.c

--- esp32-firmware/components/audio_codec/audio_codec.c
+++ esp32-firmware/components/audio_codec/audio_codec.c
@@ -66,16 +66,3 @@ int audio_codec_write(const uint8_t* buffer, size_t len) {
     esp_err_t ret = i2s_write(I2S_NUM_1, buffer, len, &bytes_written, portMAX_DELAY);
     return (ret == ESP_OK) ? bytes_written : 0;
 }
-Audiocodec.h
-#pragma once
-#include <stdint.h>
-#include <stddef.h>
-
-// Initialize mic (INMP441) and amp (MAX98357A)
-void audio_codec_init(void);
-
-// Read audio samples from mic
-int audio_codec_read(uint8_t* buffer, size_t len);
-
-// Write audio samples to amp
-int audio_codec_write(const uint8_t* buffer, size_t len);
